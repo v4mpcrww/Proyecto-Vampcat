38b83a3eb71a8eb4f154b2ef9a2c51043834c22f
v4mpcrw <gloxinias.in.my.crypt@gmail.com>
2026-01-01 22:05:15 -0300

    avances

--- php/conexion.php
+++ php/conexion.php
@@ -10,6 +10,7 @@ $conexion= pg_connect("host=$equipo dbname=$namebd port=$puerto user=$usuario pa
 if($conexion) 
 { 
     pg_set_client_encoding($conexion, "UTF8");
+    echo "conexione xitosa";
 }
 else 
 { echo "Ha ocurrido un error"; 
